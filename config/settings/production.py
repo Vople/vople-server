0710028c6fa28fd12626e52e9bc8bbf394f40319
jaemin <once29@naver.com>
2018-11-03 01:43:45 +0900

    Remove AWS_S3_OBJECT_PARAM

--- config/settings/production.py
+++ config/settings/production.py
@@ -73,9 +73,9 @@ AWS_QUERYSTRING_AUTH = False
 # DO NOT change these unless you know what you're doing.
 _AWS_EXPIRY = 60 * 60 * 24 * 7
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': f'max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate',
-}
+#AWS_S3_OBJECT_PARAMETERS = {
+#    'CacheControl': f'max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate',
+#}
 
 # STATIC
 # ------------------------

256b89815d3f18ee301651b676467ffa2d9ada3f
jaemin <once29@naver.com>
2018-11-03 03:19:25 +0900

    TEST

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,7 +4,7 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['github.com/mkSpace'])
 
@@ -76,7 +76,7 @@ _AWS_EXPIRY = 60 * 60 * 24 * 7
 #AWS_S3_OBJECT_PARAMETERS = {
 #    'CacheControl': f'max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate',
 #}
-
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 # DATABASE
 DATABASES = {
     'default': {

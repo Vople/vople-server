16c586227f6219cbc6d9f759fa709046c3923754
jaemin <once29@naver.com>
2018-11-03 05:40:34 +0900

    URL

--- config/settings/production.py
+++ config/settings/production.py
@@ -96,7 +96,7 @@ DATABASES = {
 # ------------------------
 
 STATICFILES_STORAGE = 'config.settings.production.StaticRootS3Boto3Storage'
-STATIC_URL = 'https://s3.amazonaws.com/%s/static/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = '/static/
 
 # MEDIA
 # ------------------------------------------------------------------------------
@@ -117,7 +117,7 @@ class MediaRootS3Boto3Storage(S3Boto3Storage):
 
 # endregion
 DEFAULT_FILE_STORAGE = 'config.settings.production.MediaRootS3Boto3Storage'
-MEDIA_URL = 'https://s3.amazonaws.com/%s/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '/media/'
 
 # TEMPLATES
 # ------------------------------------------------------------------------------

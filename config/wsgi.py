d8b04698dcdc4a3a7e58d29e65167a940c35ad5f
jaemin <once29@naver.com>
2018-11-03 04:28:05 +0900

    RE

--- config/wsgi.py
+++ config/wsgi.py
@@ -20,8 +20,7 @@ from django.core.wsgi import get_wsgi_application
 
 # This allows easy placement of apps within the interior
 # vople directory.
-app_path = os.path.abspath(os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), os.pardir))
+app_path = os.path.dirname(os.path.abspath(__file__)).replace('/config','')
 sys.path.append(os.path.join(app_path, 'vople'))
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks

a2d6212188e15c0e2597a140427853d29bab332b
jaemin <once29@naver.com>
2018-11-05 02:16:27 +0900

    Bug

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     ),
     path("accounts/", include("allauth.urls")),
     url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls'))
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     # Your stuff: custom urls includes go here
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT

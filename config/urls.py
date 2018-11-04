2529d4b65a857a3aceb0cad72d4ca8c3ac43c669
jaemin <once29@naver.com>
2018-11-05 03:41:05 +0900

    RE

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     url(settings.ADMIN_URL, admin.site.urls),
 
 
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
-    
+    #url(r'^rest-auth/', include('rest_auth.urls')),
+    #url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+
     # User management
     path(
         "users/",

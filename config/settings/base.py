538526a070d1542c22d7008552b41168a97063f6
jaemin <once29@naver.com>
2018-11-05 03:21:26 +0900

    RE

--- config/settings/base.py
+++ config/settings/base.py
@@ -240,3 +240,16 @@ SOCIALACCOUNT_ADAPTER = 'vople.users.adapters.SocialAccountAdapter'
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        #'rest_framework.authentication.TokenAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+REST_USE_JWT = True
